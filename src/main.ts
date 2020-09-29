0836f9417a17665a13180ad1e2930b7dc19fb6fe
Ali-Mac <ali@Ali-Macs-MacBook-Pro.local>
2020-09-29 22:14:09 +0300

    add main file , export all classes

---
+++ src/main.ts
@@ -0,0 +1,76 @@
+export * from "./ChangeHandler/AutoUploadFieldChangeHandler";
+export * from "./ChangeHandler/DefaultDynamicFieldChangeHandler";
+export * from "./ChangeHandler/DefaultFieldChangeHandler";
+
+export * from "./DefaultElement/DefaultArrayTextField";
+export * from "./DefaultElement/DefaultAutoUploadField";
+export * from "./DefaultElement/DefaultCheckboxField";
+export * from "./DefaultElement/DefaultFieldBase";
+export * from "./DefaultElement/DefaultFileField";
+export * from "./DefaultElement/DefaultOptionsFieldBase";
+export * from "./DefaultElement/DefaultPasswordField";
+export * from "./DefaultElement/DefaultRadioField";
+export * from "./DefaultElement/DefaultSelectField";
+export * from "./DefaultElement/DefaultTextAreaField";
+export * from "./DefaultElement/DefaultTextField";
+
+
+export * from "./Defaults/RafDefaults";
+export * from "./Defaults/Services/Validator";
+export * from "./Defaults/Services/DefaultFieldRenderer";
+export * from "./Defaults/Services/DefaultFormRenderer";
+export * from "./Defaults/Services/DefaultSubmitter";
+export * from "./Defaults/Services/FieldRendererBase";
+
+export * from "./Event/DefaultEvents";
+export * from "./Event/EventType";
+export * from "./Event/IEventNameMaker";
+
+
+export * from "./Field/IField";
+export * from "./Field/FieldProps";
+export * from "./Field/FieldState";
+
+export * from "./Field/FieldEvent/FieldEvent";
+export * from "./Field/Collecting/FieldCollecting";
+export * from "./Field/Collecting/FieldCollectingConfiguration";
+export * from "./Field/Concrete/FieldType";
+export * from "./Field/Concrete/DynamicField";
+export * from "./Field/Concrete/Field";
+export * from "./Field/Concrete/FieldStateInitializer";
+export * from "./Field/Concrete/FileField";
+export * from "./Field/Concrete/AutoUploadField/AutoUploadField";
+export * from "./Field/Concrete/AutoUploadField/AutoUploadFieldExtra";
+export * from "./Field/Concrete/AutoUploadField/AutoUploadFieldExtraConfiguration";
+export * from "./Field/Concrete/AutoUploadField/AutoUploader";
+export * from "./Field/Concrete/AutoUploadField/AutoUploadFieldEvents";
+export * from "./Field/Configuration/DynamicFieldExtra";
+export * from "./Field/Configuration/FieldExtra";
+export * from "./Field/Configuration/FieldConfiguration";
+export * from "./Field/UI/FieldUI";
+export * from "./Field/UI/FieldUIConfiguration";
+export * from "./Field/Validation/FieldValidation";
+export * from "./Field/Validation/FieldValidationConfiguration";
+export * from "./Field/Value/FieldValue";
+export * from "./Field/Value/FieldValueConfiguration";
+
+
+export * from "./Form/Form";
+export * from "./Form/FormEvent/FormEvent";
+export * from "./Form/FormValue/FormValue";
+export * from "./Form/FieldManager/FormFieldManager";
+export * from "./Form/FormCollecting/IFormCollector";
+export * from "./Form/FormDefault";
+export * from "./Form/FormProps";
+export * from "./Form/FormService";
+export * from "./Form/FormState";
+export * from "./Form/FormUI/FormUI";
+export * from "./Form/FormValidation/FormValidation";
+export * from "./Form/IForm";
+
+
+export * from "./Protocol/Validator";
+export * from "./Protocol/FormRenderer";
+export * from "./Protocol/FieldChangeHandler";
+export * from "./Protocol/FieldRenderer";
+export * from "./Protocol/Submitter";
