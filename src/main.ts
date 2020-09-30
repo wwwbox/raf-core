5b291bdadc738d2418e88d1c8fa6c7b581542d9e
Ali-Mac <ali@Ali-Macs-MacBook-Pro.local>
2020-09-30 22:57:39 +0300

    fixing exporting issue

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,47 @@
-export * from "./ChangeHandler/AutoUploadFieldChangeHandler";
-export * from "./ChangeHandler/DefaultDynamicFieldChangeHandler";
-export * from "./ChangeHandler/DefaultFieldChangeHandler";
-
-export * from "./DefaultElement/DefaultArrayTextField";
-export * from "./DefaultElement/DefaultAutoUploadField";
-export * from "./DefaultElement/DefaultCheckboxField";
-export * from "./DefaultElement/DefaultFieldBase";
-export * from "./DefaultElement/DefaultFileField";
-export * from "./DefaultElement/DefaultOptionsFieldBase";
-export * from "./DefaultElement/DefaultPasswordField";
-export * from "./DefaultElement/DefaultRadioField";
-export * from "./DefaultElement/DefaultSelectField";
-export * from "./DefaultElement/DefaultTextAreaField";
-export * from "./DefaultElement/DefaultTextField";
+import DefaultArrayTextField from "./DefaultElement/DefaultArrayTextField";
+import {DefaultAutoUploadField} from "./DefaultElement/DefaultAutoUploadField";
+import DefaultCheckboxField from "./DefaultElement/DefaultCheckboxField";
+import DefaultFileField from "./DefaultElement/DefaultFileField";
+import DefaultPasswordField from "./DefaultElement/DefaultPasswordField";
+import DefaultRadioField from "./DefaultElement/DefaultRadioField";
+import DefaultSelectField from "./DefaultElement/DefaultSelectField";
+import DefaultTextAreaField from "./DefaultElement/DefaultTextAreaField";
+import DefaultTextField from "./DefaultElement/DefaultTextField";
 
 
-export * from "./Defaults/RafDefaults";
-export * from "./Defaults/Services/Validator";
-export * from "./Defaults/Services/DefaultFieldRenderer";
-export * from "./Defaults/Services/DefaultFormRenderer";
-export * from "./Defaults/Services/DefaultSubmitter";
-export * from "./Defaults/Services/FieldRendererBase";
+import {NotEmptyValidator} from "./Defaults/Services/Validator";
+import DefaultFieldRenderer from "./Defaults/Services/DefaultFieldRenderer";
+import {DefaultFormRenderer} from "./Defaults/Services/DefaultFormRenderer";
+import {DefaultSubmitter} from "./Defaults/Services/DefaultSubmitter";
+import FieldRendererBase from "./Defaults/Services/FieldRendererBase";
+import DynamicField from "./Field/Concrete/DynamicField";
+import Field from "./Field/Concrete/Field";
+import FileField from "./Field/Concrete/FileField";
+import {AutoUploadField} from "./Field/Concrete/AutoUploadField/AutoUploadField";
 
-export * from "./Event/DefaultEvents";
-export * from "./Event/EventType";
-export * from "./Event/IEventNameMaker";
-
-
-export * from "./Field/IField";
-export * from "./Field/FieldProps";
-export * from "./Field/FieldState";
-
-export * from "./Field/FieldEvent/FieldEvent";
-export * from "./Field/Collecting/FieldCollecting";
-export * from "./Field/Collecting/FieldCollectingConfiguration";
-export * from "./Field/Concrete/FieldType";
-export * from "./Field/Concrete/DynamicField";
-export * from "./Field/Concrete/Field";
-export * from "./Field/Concrete/FieldStateInitializer";
-export * from "./Field/Concrete/FileField";
-export * from "./Field/Concrete/AutoUploadField/AutoUploadField";
-export * from "./Field/Concrete/AutoUploadField/AutoUploadFieldExtra";
-export * from "./Field/Concrete/AutoUploadField/AutoUploadFieldExtraConfiguration";
-export * from "./Field/Concrete/AutoUploadField/AutoUploader";
-export * from "./Field/Concrete/AutoUploadField/AutoUploadFieldEvents";
-export * from "./Field/Configuration/DynamicFieldExtra";
-export * from "./Field/Configuration/FieldExtra";
-export * from "./Field/Configuration/FieldConfiguration";
-export * from "./Field/UI/FieldUI";
-export * from "./Field/UI/FieldUIConfiguration";
-export * from "./Field/Validation/FieldValidation";
-export * from "./Field/Validation/FieldValidationConfiguration";
-export * from "./Field/Value/FieldValue";
-export * from "./Field/Value/FieldValueConfiguration";
+import Form from "./Form/Form";
 
 
-export * from "./Form/Form";
-export * from "./Form/FormEvent/FormEvent";
-export * from "./Form/FormValue/FormValue";
-export * from "./Form/FieldManager/FormFieldManager";
-export * from "./Form/FormCollecting/IFormCollector";
-export * from "./Form/FormDefault";
-export * from "./Form/FormProps";
-export * from "./Form/FormService";
-export * from "./Form/FormState";
-export * from "./Form/FormUI/FormUI";
-export * from "./Form/FormValidation/FormValidation";
-export * from "./Form/IForm";
+export {
+    DefaultArrayTextField,
+    DefaultAutoUploadField,
+    DefaultCheckboxField,
+    DefaultFileField,
+    DefaultPasswordField,
+    DefaultRadioField,
+    DefaultSelectField,
+    DefaultTextAreaField,
+    DefaultTextField,
+    NotEmptyValidator,
+    DefaultFieldRenderer,
+    DefaultFormRenderer,
+    DefaultSubmitter,
+    FieldRendererBase,
+    Form,
+    Field,
+    FileField,
+    DynamicField,
+    AutoUploadField
+}
 
 
-export * from "./Protocol/Validator";
-export * from "./Protocol/FormRenderer";
-export * from "./Protocol/FieldChangeHandler";
-export * from "./Protocol/FieldRenderer";
-export * from "./Protocol/Submitter";
