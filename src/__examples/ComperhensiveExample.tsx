1595b95783af5964da365b8466825eb8b5a28d61
Ali Faris <alifaris@earthlink.iq>
2020-12-27 19:31:37 +0300

    more elements in example

--- src/__examples/ComperhensiveExample.tsx
+++ src/__examples/ComperhensiveExample.tsx
@@ -3,6 +3,11 @@ import Form from "../Form/Form";
 import DefaultTextField from "../DefaultElement/DefaultTextField";
 import DefaultPasswordField from "../DefaultElement/DefaultPasswordField";
 import IForm from "../Form/IForm";
+import DefaultRadioField from "../DefaultElement/DefaultRadioField";
+import DefaultSelectField from "../DefaultElement/DefaultSelectField";
+import DefaultTextAreaField from "../DefaultElement/DefaultTextAreaField";
+import DefaultArrayTextField from "../DefaultElement/DefaultArrayTextField";
+import DefaultCheckboxField from "../DefaultElement/DefaultCheckboxField";
 
 
 export class ComprehensiveExample extends Component {
@@ -15,33 +20,33 @@ export class ComprehensiveExample extends Component {
                     {as: DefaultTextField, name: 'name', extra: {label: 'Name'}},
                     {as: DefaultTextField, name: 'username', extra: {label: 'Username'}},
                     {as: DefaultPasswordField, name: 'password', extra: {label: 'Password'}},
-                    // {
-                    //     as: DefaultRadioField, name: 'type', extra: {
-                    //         label: 'Type',
-                    //         options: [{label: 'Admin', value: 1}, {label: 'User', value: 2}]
-                    //     }
-                    // },
-                    // {
-                    //     as: DefaultSelectField, name: 'country', extra: {
-                    //         label: 'Country',
-                    //         options: [{label: 'Iraq', value: 1}, {label: 'Japan', value: 2}]
-                    //     }
-                    // },
-                    // {
-                    //     as: DefaultTextAreaField, name: 'message', extra: {
-                    //         label: 'Message...'
-                    //     }
-                    // },
-                    // {
-                    //     as: DefaultArrayTextField, name: 'friends', extra: {
-                    //         label: 'Enter your friends names'
-                    //     }
-                    // },
-                    // {as: DefaultCheckboxField, name: 'policy_agreement', extra: {label: 'I Accept The Policy'}},
+                    {
+                        as: DefaultRadioField, name: 'type', extra: {
+                            label: 'Type',
+                            options: [{label: 'Admin', value: 1}, {label: 'User', value: 2}]
+                        }
+                    },
+                    {
+                        as: DefaultSelectField, name: 'country', extra: {
+                            label: 'Country',
+                            options: [{label: 'Iraq', value: 1}, {label: 'Japan', value: 2}]
+                        }
+                    },
+                    {
+                        as: DefaultTextAreaField, name: 'message', extra: {
+                            label: 'Message...'
+                        }
+                    },
+                    {
+                        as: DefaultArrayTextField, name: 'friends', extra: {
+                            label: 'Enter your friends names'
+                        }
+                    },
+                    {as: DefaultCheckboxField, name: 'policy_agreement', extra: {label: 'I Accept The Policy'}},
                 ]} extra={{
                     submitOptions: {
                         url: "http://google.com",
-                        method: "post"
+                        method: "get"
                     }
                 }}/>
                 <button onClick={() => {
@@ -51,8 +56,6 @@ export class ComprehensiveExample extends Component {
                     SET GLOBAL FORM INSTANCE
                 </button>
             </div>
-
-
         );
     }
 }
